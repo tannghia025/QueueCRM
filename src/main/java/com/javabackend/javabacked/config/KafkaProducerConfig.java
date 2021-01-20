199fb195457378f7668b3c7f2e357f427f6dc468
Tấn Nghĩa <nghia02584@gmail.com>
2021-01-20 13:19:21 +0700

    alala

--- src/main/java/com/javabackend/javabacked/config/KafkaProducerConfig.java
+++ src/main/java/com/javabackend/javabacked/config/KafkaProducerConfig.java
@@ -16,17 +16,21 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import com.javabackend.javabacked.model.User;
 
 
 @EnableKafka
 @Configuration
 public class KafkaProducerConfig {
 	@Bean
-	public ProducerFactory<String, String> producerFactory() {
+	public ProducerFactory<String, User> producerFactory() {
 		Map<String, Object> configProps = new HashMap<>();
 		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 		return new DefaultKafkaProducerFactory<>(configProps);
 	}
 	@Bean
@@ -40,20 +44,37 @@ public class KafkaProducerConfig {
 		props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
 		props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
 		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-		return new DefaultKafkaConsumerFactory<>(props);
+		return new DefaultKafkaConsumerFactory<>( props);
 	}
+	public ConsumerFactory<String, User> userConsumerFactory() {
+	    Map<String, Object> props = new HashMap<>();
+	    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,  "localhost:9092");
+	    props.put(ConsumerConfig.GROUP_ID_CONFIG, "group-id");
+	    
+	    return new DefaultKafkaConsumerFactory<>(
+	      props,
+	      new StringDeserializer(),
+	      new JsonDeserializer<>(User.class));
+	  }
 
 	@Bean
-	public KafkaTemplate<String, String> kafkaTemplate() {
+	public KafkaTemplate<String, User> kafkaTemplate() {
 		return new KafkaTemplate<>(producerFactory());
 	}
 	@Bean
 	public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
 		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
 		factory.setConsumerFactory(consumerFactory());
-		factory.setRecordFilterStrategy(record -> record.value().contains("ok"));
+		factory.setRecordFilterStrategy(record -> record.value().contains("asea"));
 		factory.setReplyTemplate(kafkaTemplate());
 		return factory;
 	}
+	 @Bean
+	  public ConcurrentKafkaListenerContainerFactory<String, User> userKafkaListenerContainerFactory() {
+	    ConcurrentKafkaListenerContainerFactory<String, User> factory =
+	      new ConcurrentKafkaListenerContainerFactory<>();
+	    factory.setConsumerFactory(userConsumerFactory());
+	    return factory;
+	  }
 	
 }
--- src/main/java/com/javabackend/javabacked/config/KafkaSenderExample.java
+++ src/main/java/com/javabackend/javabacked/config/KafkaSenderExample.java
@@ -7,24 +7,24 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import com.javabackend.javabacked.model.User;
+
 
 @Component
 public class KafkaSenderExample {
 	@Autowired
-	private KafkaTemplate<String, String> kafkaTemplate;
+	private KafkaTemplate<String, User> kafkaTemplate;
 
-	public void send(String topicName, String message) {
-		ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(topicName, message);
+	public void send(String topicName,int partition,String key, User message) {
+		ListenableFuture<SendResult<String,User>> future = kafkaTemplate.send(topicName, partition, key, message);
 
-		future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+		future.addCallback(new ListenableFutureCallback<SendResult<String, User>>() {
 			@Override
-			public void onSuccess(SendResult<String, String> result) {
+			public void onSuccess(SendResult<String, User> result) {
 				System.out.println("offset: "+result.getRecordMetadata().offset());
 				System.out.println("partitions: "+result.getRecordMetadata().partition());
 				System.out.println("Topic: "+result.getRecordMetadata().topic());
-				System.out.println("TOpic: "+result.getProducerRecord().topic());
 			}
-
 			@Override
 			public void onFailure(Throwable ex) {
 				System.out.println("offset failed check again ");
