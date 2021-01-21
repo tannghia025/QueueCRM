a8650195b40af89e7a14f789637191aa031e2959
Tấn Nghĩa <nghia02584@gmail.com>
2021-01-21 11:29:05 +0700

    ...

--- src/main/java/com/crm/crmqueue/service/OrderService.java
+++ src/main/java/com/crm/crmqueue/service/OrderService.java
@@ -2,6 +2,7 @@ package com.crm.crmqueue.service;
 
 import org.springframework.stereotype.Service;
 
+import com.crm.crmqueue.config.LineNotify;
 import com.crm.crmqueue.crmbusiness.Utils;
 import com.crm.crmqueue.dto.SaleOrderOnline2;
 import com.crm.crmqueue.dto.SaleOrderResult;
@@ -32,41 +33,59 @@ public class OrderService implements IOrderService {
 		if (objSaleOrderWeb.DELIVERYTYPEID == 101)
 			objSaleOrderWeb.DELIVERYTYPEID = 2;
 		// hàm kiểm tra thông tin khách hàng
-		infoCustomer(objSaleOrderWeb);
+//		infoCustomer(objSaleOrderWeb);
 
 		return null;
 	}
 
-	private void infoCustomer(SaleOrderOnline2 objSaleOrderWeb) {
-		// Chuẩn hóa số Phone
-		objSaleOrderWeb.Phone = objSaleOrderWeb.Phone.trim();
-		objSaleOrderWeb.CONTACTMOBILE = objSaleOrderWeb.CONTACTMOBILE.trim();
+//	private void infoCustomer(SaleOrderOnline2 objSaleOrderWeb) {
+//		// Chuẩn hóa số Phone
+//		objSaleOrderWeb.Phone = objSaleOrderWeb.Phone.trim();
+//		objSaleOrderWeb.CONTACTMOBILE = objSaleOrderWeb.CONTACTMOBILE.trim();
+//
+//		// Kiểm tra hợp lệ
+//		if (!Utils.IsValidPhoneNumber(objSaleOrderWeb.Phone, objSaleOrderWeb.GENCOMPANYID))
+//			objSaleOrderWeb.Phone = "";
+//
+//		if (Strings.isNullOrEmpty(objSaleOrderWeb.Phone)) {
+//			return InitResultErrorLog(LangHelper.GetText(LangConst.Order.CRM_ORDER_INVALIDPHONE, language), json,
+//					"Số điện thoại chưa có hoặc không hợp lệ!", objSaleOrderWeb.GENCOMPANYID);
+//		}
+//	}
+//
+//	private static SaleOrderResult InitResultErrorLog(String strMess,
+//	            String strOrderJson,
+//	            String strTitleLog,
+//	            int iGenCompanyID,
+//	            bool isRollBackCoupon = true)
+//	        {
+//
+//	            // LineMessage.SendMessage(1, "Lỗi tạo đơn hàng: " + strMess + " - " + strOrderJson);
+//
+//
+//	            SaleOrderResult objSaleOrderResult = new SaleOrderResult();
+//	            CRM.LogHelper.LogUtility.Current.AddLog("CreateOrder: " + strTitleLog, strOrderJson, "CRM_FunctionGlobals -> CreateSaleOrder_Obect", "CRMWeb.Services");
+//	            objSaleOrderResult.SALEORDERONLINEID = -1;
+//	            objSaleOrderResult.MESSSAGE = strMess;
+//
+//	            return objSaleOrderResult;
+//	        }
 
-		// Kiểm tra hợp lệ
-		if (!Utils.IsValidPhoneNumber(objSaleOrderWeb.Phone, objSaleOrderWeb.GENCOMPANYID))
-			objSaleOrderWeb.Phone = "";
+	public void kiemTraQuanHuyen(SaleOrderOnline2 objSaleOrderWeb) {
+		if (objSaleOrderWeb.getDELIVERYTYPEID() == 2 || objSaleOrderWeb.getGENCOMPANYID() == 3) {
+			if (objSaleOrderWeb.SHIPSTATE > 0 && objSaleOrderWeb.SHIPCITY > 0) {
+				// check từ cache
+			}
+			if (objSaleOrderWeb.SHIPSTATE > 0 && objSaleOrderWeb.SHIPCITY > 0) {
+				// check từ cache
+			}
 
-		if (Strings.isNullOrEmpty(objSaleOrderWeb.Phone)) {
-			return InitResultErrorLog(LangHelper.GetText(LangConst.Order.CRM_ORDER_INVALIDPHONE, language), json,
-					"Số điện thoại chưa có hoặc không hợp lệ!", objSaleOrderWeb.GENCOMPANYID);
 		}
+		//Kiểm tra có địa chỉ giao hàng không
+		if(objSaleOrderWeb.SHIPCITY > 0&& objSaleOrderWeb.SHIPSTATE <= 0 && objSaleOrderWeb.SHIPWARDID <=0) {
+			
+			LineNotify.Notify("\"Vui lòng nhập thêm số nhà, tên đường\"");
+		}
+
 	}
-
-	private static SaleOrderResult InitResultErrorLog(String strMess,
-	            String strOrderJson,
-	            String strTitleLog,
-	            int iGenCompanyID,
-	            bool isRollBackCoupon = true)
-	        {
-
-	            // LineMessage.SendMessage(1, "Lỗi tạo đơn hàng: " + strMess + " - " + strOrderJson);
-
-
-	            SaleOrderResult objSaleOrderResult = new SaleOrderResult();
-	            CRM.LogHelper.LogUtility.Current.AddLog("CreateOrder: " + strTitleLog, strOrderJson, "CRM_FunctionGlobals -> CreateSaleOrder_Obect", "CRMWeb.Services");
-	            objSaleOrderResult.SALEORDERONLINEID = -1;
-	            objSaleOrderResult.MESSSAGE = strMess;
-
-	            return objSaleOrderResult;
-	        }
 }
